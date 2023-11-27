ff8b09d403ae652c106ac14bfcdae303983105e2
rmedejesus <rme.dejesus@gmail.com>
2023-11-27 12:05:09 +0900

    Removing default components

--- src/home/App.js
+++ src/home/App.js
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,5 +18,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
